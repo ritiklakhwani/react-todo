270ecf9bb8c98422265e85e118753b41cf40a1f7
ritiklakhwani <ritiklakhwani28@gmail.com>
2025-12-27 06:29:10 +0530

    feat: get todos functionality added

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,18 @@
 require("dotenv").config();
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const { boolean } = require("zod");
 const connect = process.env.MONGO_URI;
 
-mongoose.connect(connect)
+mongoose.connect(connect);
 
 const todoSchema = new mongoose.Schema({
+  title: { type: String },
+  description: { type: String },
+  completed: { type: Boolean },
+});
 
-})
-
-const todo = mongoose.model("Todo", todoSchema)
+const todo = mongoose.model("Todo", todoSchema);
 
 module.exports = {
-    todo
-}
+  todo,
+};
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./zod");
+const { todo } = require("./db");
 const app = express();
 
 app.use(express.json());
@@ -8,17 +9,39 @@ app.post("/todo", async (req, res) => {
   const parsed = createTodo.safeParse(req.body);
   if (!parsed.success) {
     return res.status(400).json({
-      success: "false",
+      success: false,
       msg: "invalid input schema!",
     });
   }
 
   const data = parsed.data;
 
-  //add in mongo
+  const Todo = await todo.create({
+    title: data.title,
+    description: data.description,
+    completed: data.completed
+  })
+
+  res.status(200).json({
+    success: true,
+    msg: "todo added!"
+  })
 });
 
-app.get("/todos", (req, res) => {});
+app.get("/todos", async(req, res) => {
+    const todos = await todo.find({});
+    if(!todos){
+        return res.status(400).json({
+            success: false,
+            msg: "todos not found!"
+        })
+    }
+
+    res.status(201).json({
+        success: true,
+        todos: todos
+    })
+});
 
 app.put("/completed", (req, res) => {
   const parsed = updateTodo.safeParse(req.body);
